d1d9b068640bfa1678759c96413f63161778c7bf
funan <funan.zsw@dtyunxi.com>
2019-06-01 10:18:58 +0800

    '添加sass支持'

--- src/pages/AntdAlert.js
+++ src/pages/AntdAlert.js
@@ -1,11 +1,28 @@
 import React from 'react'
-import { Icon, Button, Select, Divider, Alert, Drawer, Radio, Modal, message, notification, Progress, Popconfirm, Spin, Skeleton, Switch, } from 'antd'
+import {
+    Icon,
+    Button,
+    Select,
+    Divider,
+    Alert,
+    Drawer,
+    Radio,
+    Modal,
+    message,
+    notification,
+    Progress,
+    Popconfirm,
+    Spin,
+    Skeleton,
+    Switch
+} from 'antd'
+import './AntdAlert.scss'
 
-const { Option } = Select;
+const { Option } = Select
 const onClose = e => {
-    console.log(e, 'I was closed.');
-};
-const options = ['topLeft', 'topRight', 'bottomLeft', 'bottomRight'];
+    console.log(e, 'I was closed.')
+}
+const options = ['topLeft', 'topRight', 'bottomLeft', 'bottomRight']
 
 class AntdAlert extends React.Component {
     state = {
@@ -52,14 +69,18 @@ class AntdAlert extends React.Component {
         //     .loading('Action in progress..', 2.5)
         //     .then(() => message.success('Loading finished', 2.5))
         //     .then(() => message.error('Loading finished is finished', 2.5));
-        const hide = message.loading('Action in progress..', 0);
+        const hide = message.loading('Action in progress..', 0)
         // Dismiss manually and asynchronously
-        setTimeout(hide, 2500);
+        setTimeout(hide, 2500)
     }
     showNotification = () => {
         const key = `open${Date.now()}`
         const btn = (
-            <Button type="primary" size="small" onClick={() => notification.close(key)}>
+            <Button
+                type="primary"
+                size="small"
+                onClick={() => notification.close(key)}
+            >
                 Confirm
             </Button>
         )
@@ -72,7 +93,7 @@ class AntdAlert extends React.Component {
             icon: <Icon type="smile" style={{ color: '#108ee9' }} />,
             style: {
                 width: 600,
-                marginLeft: 335 - 600,
+                marginLeft: 335 - 600
             },
             onClick: () => {
                 console.log('点击notification通知')
@@ -83,22 +104,22 @@ class AntdAlert extends React.Component {
                 key,
                 message: '通过唯一的key修改内容'
             })
-        }, 1000);
+        }, 1000)
     }
     increase = () => {
-        let percent = this.state.percent + 10;
+        let percent = this.state.percent + 10
         if (percent > 100) {
-            percent = 100;
+            percent = 100
         }
-        this.setState({ percent });
-    };
+        this.setState({ percent })
+    }
     decline = () => {
-        let percent = this.state.percent - 10;
+        let percent = this.state.percent - 10
         if (percent < 0) {
-            percent = 0;
+            percent = 0
         }
-        this.setState({ percent });
-    };
+        this.setState({ percent })
+    }
     handlePopconfirmConfirm = () => {
         console.log('yes')
         message.success('yes')
@@ -112,29 +133,58 @@ class AntdAlert extends React.Component {
             <div>
                 <div className="antd-demo-box">
                     <h1>alert-警告提示</h1>
-                    <p>
-
-                    </p>
+                    <p />
                     <div className="antd-demo-content">
                         <Alert message="SUCCESS text" type="success" />
-                        <Alert message="alert消息" description="alert详细描述" type="error" closable onClose={onClose} />
-                        <Alert message="alert消息" description="alert详细描述" type="error" closable onClose={onClose} closeText="关闭" />
-                        <Alert message="alert消息" description="alert详细描述" type="info" showIcon banner />
+                        <Alert
+                            message="alert消息"
+                            description="alert详细描述"
+                            type="error"
+                            closable
+                            onClose={onClose}
+                        />
+                        <Alert
+                            message="alert消息"
+                            description="alert详细描述"
+                            type="error"
+                            closable
+                            onClose={onClose}
+                            closeText="关闭"
+                        />
+                        <Alert
+                            message="alert消息"
+                            description="alert详细描述"
+                            type="info"
+                            showIcon
+                            banner
+                        />
                     </div>
                 </div>
                 <Divider />
-                <div className="antd-demo-bo">
+                <div className="antd-demo-box demo">
                     <h1>drawer-抽屉</h1>
-                    <p></p>
+                    <p />
                     <div className="antd-demo-content">
-                        <Radio.Group defaultValue={this.state.drawerPlacement} onChange={this.handleDrawerPlacementChange} style={{ marginRight: '8px' }}>
+                        <Radio.Group
+                            defaultValue={this.state.drawerPlacement}
+                            onChange={this.handleDrawerPlacementChange}
+                            style={{ marginRight: '8px' }}
+                        >
                             <Radio value="top">上</Radio>
                             <Radio value="right">右</Radio>
                             <Radio value="bottom">下</Radio>
                             <Radio value="left">左</Radio>
                         </Radio.Group>
-                        <Button type="primary" onClick={this.showDrawer}>打开抽屉</Button>
-                        <Drawer title="drawer" placement={this.state.drawerPlacement} onClose={this.handleDrawerClose} visible={this.state.drawerVisible} closable={false}>
+                        <Button type="primary" onClick={this.showDrawer}>
+                            打开抽屉
+                        </Button>
+                        <Drawer
+                            title="drawer"
+                            placement={this.state.drawerPlacement}
+                            onClose={this.handleDrawerClose}
+                            visible={this.state.drawerVisible}
+                            closable={false}
+                        >
                             <p>111111</p>
                             <p>111111</p>
                             <p>111111</p>
@@ -144,12 +194,17 @@ class AntdAlert extends React.Component {
                 <Divider />
                 <div className="antd-demo-box">
                     <h1>modal-对话框</h1>
-                    <p></p>
+                    <p />
                     <div className="antd-demo-content">
                         <Button type="primary" onClick={this.showModal}>
                             打开对话框
                         </Button>
-                        <Modal title="对话框" visible={this.state.modalVisible} onOk={this.handleModalOk} onCancel={this.handleModalCancel}>
+                        <Modal
+                            title="对话框"
+                            visible={this.state.modalVisible}
+                            onOk={this.handleModalOk}
+                            onCancel={this.handleModalCancel}
+                        >
                             <p>111111111</p>
                             <p>111111111</p>
                             <p>111111111</p>
@@ -160,23 +215,25 @@ class AntdAlert extends React.Component {
                 <Divider />
                 <div className="antd-demo-box">
                     <h1>message-全局提示</h1>
-                    <p></p>
+                    <p />
                     <div className="antd-demo-content">
-                        <Button type="primary" onClick={this.showMessage}>发送提示</Button>
+                        <Button type="primary" onClick={this.showMessage}>
+                            发送提示
+                        </Button>
                     </div>
                 </div>
                 <Divider />
                 <div className="antd-demo-box">
                     <h1>nitification-通知提醒框</h1>
-                    <p></p>
+                    <p />
                     <div className="antd-demo-content">
                         <Select
                             defaultValue="topRight"
                             style={{ width: 120, marginRight: 10 }}
                             onChange={val => {
                                 notification.config({
-                                    placement: val,
-                                });
+                                    placement: val
+                                })
                             }}
                         >
                             {options.map(val => (
@@ -185,32 +242,54 @@ class AntdAlert extends React.Component {
                                 </Option>
                             ))}
                         </Select>
-                        <Button type="primary" onClick={this.showNotification}>发送通知</Button>
+                        <Button type="primary" onClick={this.showNotification}>
+                            发送通知
+                        </Button>
                     </div>
                 </div>
                 <Divider />
                 <div className="antd-demo-box">
                     <h1>progress-进度条</h1>
-                    <p></p>
+                    <p />
                     <div className="antd-demo-content">
                         <Progress percent={35} status="active" />
-                        <Progress percent={70} status="exception" size="small" />
-                        <Progress percent={70} showInfo={false} strokeColor={{ '0%': '#108ee9', '100%': '#87d068' }} />
+                        <Progress
+                            percent={70}
+                            status="exception"
+                            size="small"
+                        />
+                        <Progress
+                            percent={70}
+                            showInfo={false}
+                            strokeColor={{ '0%': '#108ee9', '100%': '#87d068' }}
+                        />
                         <div>
-                            <Progress type="circle" percent={this.state.percent} width={80} />
+                            <Progress
+                                type="circle"
+                                percent={this.state.percent}
+                                width={80}
+                            />
                             <Button.Group>
                                 <Button icon="minus" onClick={this.decline} />
                                 <Button icon="plus" onClick={this.increase} />
                             </Button.Group>
                         </div>
-                        <Progress percent={34} format={(percent) => `${percent} Days`} type="dashboard" />
-                        <Progress percent={34} successPercent={22} strokeLinecap="square" />
+                        <Progress
+                            percent={34}
+                            format={percent => `${percent} Days`}
+                            type="dashboard"
+                        />
+                        <Progress
+                            percent={34}
+                            successPercent={22}
+                            strokeLinecap="square"
+                        />
                     </div>
                 </div>
                 <Divider />
                 <div className="antd-demo-box">
                     <h1>popconfirm-气泡确认框</h1>
-                    <p></p>
+                    <p />
                     <div className="antd-demo-content">
                         <Popconfirm
                             title="确定删除？"
@@ -219,7 +298,12 @@ class AntdAlert extends React.Component {
                             placement="left"
                             onConfirm={this.handlePopconfirmConfirm}
                             onCancel={this.handlePopconfirmCancel}
-                            icon={<Icon type="question-circle-o" style={{ color: 'red' }} />}
+                            icon={
+                                <Icon
+                                    type="question-circle-o"
+                                    style={{ color: 'red' }}
+                                />
+                            }
                         >
                             <Button>Delete</Button>
                         </Popconfirm>
@@ -228,34 +312,61 @@ class AntdAlert extends React.Component {
                 <Divider />
                 <div className="antd-demo-box">
                     <h1>spin-加载中</h1>
-                    <p></p>
+                    <p />
                     <div className="antd-demo-content">
                         <Spin />
-                        <div style={{ textAlign: 'center', background: 'rgba(0, 0, 0, 0.05)', borderRadius: '4px', marginBottom: '20px', padding: '30px 50px', margin: '20px 0' }}>
+                        <div
+                            style={{
+                                textAlign: 'center',
+                                background: 'rgba(0, 0, 0, 0.05)',
+                                borderRadius: '4px',
+                                marginBottom: '20px',
+                                padding: '30px 50px',
+                                margin: '20px 0'
+                            }}
+                        >
                             <Spin size="large" />
                         </div>
-                        <Spin tip="Loading..." spinning={this.state.loading} delay={500}>
+                        <Spin
+                            tip="Loading..."
+                            spinning={this.state.loading}
+                            delay={500}
+                        >
                             <Alert
                                 message="Alert message title"
                                 description="Further details about the context of this alert."
                                 type="info"
                             />
                         </Spin>
-                        <div style={{marginTop: '16px'}}>
-                            Loading state: <Switch checked={this.state.loading} onChange={value => {this.setState({loading: value})}} />
+                        <div style={{ marginTop: '16px' }}>
+                            Loading state:{' '}
+                            <Switch
+                                checked={this.state.loading}
+                                onChange={value => {
+                                    this.setState({ loading: value })
+                                }}
+                            />
                         </div>
-                        <Spin indicator={<Icon type="loading" style={{ fontSize: 24 }} spin />} />
+                        <Spin
+                            indicator={
+                                <Icon
+                                    type="loading"
+                                    style={{ fontSize: 24 }}
+                                    spin
+                                />
+                            }
+                        />
                     </div>
                 </div>
                 <Divider />
                 <div className="antd-demo-box">
                     <h1>skeleton-骨架屏</h1>
-                    <p></p>
+                    <p />
                     <div className="antd-demo-content">
                         <Skeleton />
                     </div>
                 </div>
-            </div >
+            </div>
         )
     }
 }
