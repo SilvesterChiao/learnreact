877bee8aa10b7bf182c5835693bdb4c3498e276d
zhao.shiwei <funan.zsw@dtyunxi.com>
2019-06-03 11:52:43 +0800

    '添加sass,打包为docs目录

---
+++ docs/precache-manifest.dcf4a0cd9a8e327209fac2d7dab567db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9876abd222dbdfed4b5e6b7c6843e83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94dc4e206db4648d2dc7",
+    "url": "/static/css/2.a414b599.chunk.css"
+  },
+  {
+    "revision": "e3d97b65d42f3a64cc99",
+    "url": "/static/css/main.fab01b22.chunk.css"
+  },
+  {
+    "revision": "94dc4e206db4648d2dc7",
+    "url": "/static/js/2.77f85018.chunk.js"
+  },
+  {
+    "revision": "e3d97b65d42f3a64cc99",
+    "url": "/static/js/main.5f0095a7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
